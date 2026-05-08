327a3f901e7e9fa8f7d4698483356aa2b2fe1d75
Ayrat <harisoff156@gmail.com>
2026-05-08 07:29:18 +0300

    refactor code

--- JavaScript/homework-9.js
+++ JavaScript/homework-9.js
@@ -1,80 +1,22 @@
-const emailForm = document.querySelector('.subscribe-form');
+import { Modal } from "./modal.js";
+import { Form } from "./form.js";
 
-const openRegistrationBtn = document.getElementById('open-registration-button');
-const modal = document.querySelector('.modal');
-const closeRegistrationBtn = document.querySelector('.close-register-button');
-const overlay = document.querySelector('.overlay');
-const modalForm = document.querySelector('.modal-form');
-const inputPassword = document.getElementById('password-input');
-const inputPasswordRepeat = document.getElementById('password-repeat-input');
-const submitRegistrationBtn = document.getElementById('registration-done');
-
-const modalReject = document.querySelector('.registration-reject');
-
-const modalData = new FormData(modalForm);
-const data = Object.fromEntries(modalData.entries());
-
-const user = { ...data, createdOn: new Date() };
+const modal = new Modal(document.getElementById('modal-wrapper'));
+const modalForm = new Form('.modal-form');
+const emailForm = new Form('.subscribe-form');
 
 // задание №4 добавление логики к футеру
 
-emailForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const emailData = new FormData(emailForm);
-  const obj = Object.fromEntries(emailData.entries());
-  console.log(obj);
-});
+emailForm.submitFooter();
 
 // задание №5 логика для открытия-закрытия модального окна
 
-// openRegistrationBtn.addEventListener('click', () => {
-//   modal.classList.add('modal-showed');
-//   overlay.style.display = 'block';
-// });
-
-closeRegistrationBtn.addEventListener('click', () => {
-  modal.classList.remove('modal-showed');
-  overlay.style.display = 'none';
-});
+modal.openButton();
+modal.closeButton();
 
 // задание №6 логика работы с модальным окном
-
-inputPassword.addEventListener('input', () => {
-  if (inputPassword.validity.patternMismatch) {
-    inputPassword.setCustomValidity('Пароль должен содержать минимум 8 символов, хотя бы одну цифру и одну букву!');
-  } else {
-    inputPassword.setCustomValidity('');
-  }
-});
-
-inputPasswordRepeat.addEventListener('input', () => {
-  if (inputPassword.value !== inputPasswordRepeat.value) {
-    inputPasswordRepeat.setCustomValidity('Пароли не совпадают!');
-  } else {
-    inputPasswordRepeat.setCustomValidity('');
-  }
-});
-
-inputPassword.addEventListener('input', () => {
-  if (inputPassword.value !== inputPasswordRepeat.value) {
-    inputPasswordRepeat.setCustomValidity('Пароли не совпадают!');
-  } else {
-    inputPasswordRepeat.setCustomValidity('');
-  }
-});
+modalForm.submit(() => { modal.closeModal() });
 
 
-submitRegistrationBtn.addEventListener('click', () => {
-  if (modalForm.checkValidity()) {
-    modalForm.addEventListener('submit', () => {
-      event.preventDefault();
-      const modalData = new FormData(modalForm);
-      const data = Object.fromEntries(modalData.entries());
-      modal.classList.remove('modal-showed');
-      overlay.style.display = 'none';
-      const updatedData = { ...data, createdOn: new Date() };
-      console.log(updatedData);
-    });
-  } else confirm('Регистрация не удалась!');
-});
+
 
